1362aaac638040b85533b44fb1b26d33ff8be598
Andrey <krivochenko_andrey@mail.ru>
2022-01-02 21:17:04 +0700

    2-й практический урок

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 const personalMovieDb = {
     count: numberOfFilms,
@@ -10,7 +12,19 @@ const personalMovieDb = {
 for (let i = 0; i < numberOfFilms; i++) {
     const films = prompt("Один из последних просмотренных фильмов?", ""),
           ball = prompt("На сколько оцените его?", "");
+    if ((films === "" || ball === "" || films === null || ball === null) || (films.length > 50)) {
+        i--;
+        continue;
+    }
     personalMovieDb.movies[films] = ball;
+}
+
+if (personalMovieDb.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDb.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Вы классический зритель");
 }
 
 console.log(personalMovieDb);
